32ad711433af1d8329896226e9b13d01229b569d
Matheus Gouvea <matheusdossantosgouvea@gmail.com>
2025-02-15 17:16:27 -0300

    nova aula

---
+++ Estruturadecisao/Programa4.java
@@ -0,0 +1,20 @@
+package Estruturadecisao;
+
+public class Programa4 {
+
+    public static void main(String[] args){
+
+        //Declarando e inicializando a variavel
+        int numero = 3;
+
+        if (numero > 5){
+            System.out.println("Sim, o número " + numero + " é maior que 5");
+        } else if (numero == 5) {
+            System.out.println("O número " + numero + " é igual a 5");
+        } else {
+            System.out.println("O número " + numero + " não é menor que 5");
+        }
+
+    }
+
+}

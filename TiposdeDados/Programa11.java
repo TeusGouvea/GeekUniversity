9bd97352e6a48976c6cbea4fea59688ddcb5d584
Matheus Gouvea <matheusdossantosgouvea@gmail.com>
2025-03-03 17:28:21 -0300

    aulas 11 e 12 do modulo tipos de dados.

---
+++ TiposdeDados/Programa11.java
@@ -0,0 +1,26 @@
+package TiposdeDados;
+
+//Tipos de dados
+//Numéricos(Inteiros e Reais)
+
+public class Programa11 {
+
+    public static void main(String[] args) {
+
+        //Tipos primários
+        int num1 = 4; //Inteiro
+        short num2 = 4; //Inteiro(curto/menor/baixo)
+
+
+        //Tipos não primários
+        Integer num3 = 98;
+        short num4 = 7;
+
+        System.out.println("int -> Num1 = " + num1);
+        System.out.println("short -> Num2 = " + num2);
+        System.out.println("Integer -> Num3 = " + num3);
+        System.out.println("Short -> Num4 = " + num4);
+
+    }
+
+}
---
+++ TiposdeDados/Programa12.java
@@ -0,0 +1,27 @@
+package TiposdeDados;
+
+//Tipos de dados
+//Numéricos(Inteiros e Reais)
+
+public class Programa12 {
+
+    public static void main(String[] args) {
+
+        //Tipos primários/primitivos
+        //Por padrão, os números reais em Java são considerados double
+        float preco1 = 23.4F; //Serve para valores menores tipo preços.
+        double preco2 = 23.4; //Serve para valores maiores.
+
+        //Tipos não primários/primitivos
+        Float preco3 = 44.5f;
+        Double preco4 = 44.5;
+
+        System.out.println("float -> preco1 = " + preco1);
+        System.out.println("double -> preco2 = " + preco2);
+        System.out.println("Float -> preco3 = " + preco3);
+        System.out.println("Double -> preco4 = " + preco4);
+
+
+    }
+
+}
